65aa1e424f1cb2071bdab1ba04de496707aaa837
himang7740 <onemanwarm11@gmail.com>
2025-11-25 23:05:52 +0530

    Add files via upload

---
+++ Assignment_1/Question_2.cpp
@@ -0,0 +1,52 @@
+/*2) Design the logic to remove the duplicate elements from an Array and
+after the deletion the array should contain the unique elements*/
+#include <bits/stdc++.h>
+using namespace std;
+
+void deleteEle(int arr[100], int &n, int pos)
+{
+    if (n == 0)
+    {
+        cout << "Array is empty\n";
+        return;
+    }
+    if (pos < 0 || pos >= n)
+    {
+        cout << "Invalid position.\n";
+        return;
+    }
+    for (int i = pos; i < n - 1; i++)
+        arr[i] = arr[i + 1];
+    n--;
+}
+
+int main()
+{
+    int arr[100], n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "Array Before Removing Duplicates:\n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                deleteEle(arr, n, j);
+                j--; // stay at same index to check new shifted element
+            }
+        }
+    }
+
+    cout << "\nArray after Removing Duplicates:\n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
---
+++ Assignment_1/Question_5.cpp
@@ -0,0 +1,38 @@
+/*5) Write a program to find sum of every row and every column in a two-dimensional array.*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int row, col;
+    cout << "Enter no of rows and columns: " << endl;
+    cin >> row >> col;
+    int a[row][col];
+    cout << "Enter elements in " << row << "*" << col << " matrix : " << endl;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    for (int i = 0; i < row; i++)
+    {
+        int rowsum = 0;
+        for (int j = 0; j < col; j++)
+        {
+            rowsum += a[i][j];
+        }
+        cout << "Row " << i + 1 << " Sum is : " << rowsum << endl;
+    }
+
+    for (int j = 0; j < col; j++)
+    {
+        int colsum = 0;
+        for (int i = 0; i < row; i++)
+        {
+            colsum += a[i][j];
+        }
+        cout << "Column " << j + 1 << " Sum is : " << colsum << endl;
+    }
+}
